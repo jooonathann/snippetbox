332c7eb0a0f63fb3453ad2df2d4d610f8c55f40e
Jonathan <jooni.escobar.98@gmail.com>
2020-07-30 17:30:22 -0300

    add

---
+++ cmd/web/handlers.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"jonathhhn/snippetbox/pkg/models"
+	"net/http"
+	"strconv"
+)
+
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		app.notFound(w)
+		return
+	}
+
+	s, err := app.snippets.Latest()
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+	for _, snippet := range s {
+		fmt.Fprintf(w, "%v\n", snippet)
+	}
+
+	//	files := []string{
+	//		"./ui/html/home.page.tmpl",
+	//		"./ui/html/base.layout.tmpl",
+	//		"./ui/html/footer.partial.tmpl",
+	//	}
+	//	ts, err := template.ParseFiles(files...)
+	//	if err != nil {
+	//		app.serverError(w, err)
+	//		return
+	//	}
+	//
+	//	err = ts.Execute(w, nil)
+	//	if err != nil {
+	//		app.serverError(w, err)
+	//	}
+	//
+}
+
+func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		app.notFound(w)
+		return
+	}
+	s, err := app.snippets.Get(id)
+	if err == models.ErrNoRecord {
+		app.notFound(w)
+		return
+	} else if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	data := &templateData{Snippet: s}
+
+	files := []string{
+		"./ui/html/show.page.tmpl",
+		"./ui/html/base.layout.tmpl",
+		"./ui/html/footer.partial.tmpl",
+	}
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	err = ts.Execute(w, data)
+	if err != nil {
+		app.serverError(w, err)
+	}
+}
+
+func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.Header().Set("Allow", "POST")
+		app.clientError(w, http.StatusMethodNotAllowed)
+		return
+	}
+
+	title := "O snail"
+	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
+	expires := "7"
+
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	http.Redirect(w, r, fmt.Sprintf("/snippet?id=%d", id), http.StatusSeeOther)
+
+}
---
+++ cmd/web/templates.go
@@ -0,0 +1,7 @@
+package main
+
+import "jonathhhn/snippetbox/pkg/models"
+
+type templateData struct {
+	Snippet *models.Snippet
+}
